5bb3d2331b8569e2b9e6187fc16b0dc9319ddc52
Anthony Rahajarison <anthony.rahajarison@laplateforme.io>
2025-06-30 17:14:45 +0200

    feat : add connexion to db

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,19 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Dotenv dotenv = Dotenv.load();
+        String url = "jdbc:postgresql://localhost:5432/testdb";
+        String user = dotenv.get("PGUSER");
+        String password = dotenv.get("PGPASSWORD");
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connexion réussie !");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
