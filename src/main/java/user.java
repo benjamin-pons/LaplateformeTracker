43625ae5b28a484c2fd8727b2d62e2c38b04e926
benjamin-pons <benjamin.pons@laplateforme.io>
2025-07-02 15:28:17 +0200

    fix : rename User class and file

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -1,9 +1,9 @@
-public class user {
+public class User {
     private int id;
     private String username;
     private String password;
 
-    public user(int id, String username, String password) {
+    public User(int id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
